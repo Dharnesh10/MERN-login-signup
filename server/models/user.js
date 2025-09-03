836815ae9b9f31fd55b2c5313d8ea67a6d80491a
Dharnesh10 <dharneshk10@gmail.com>
2025-09-03 11:27:35 +0530

    last name min character updated to 1

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const userSchema = new mongoose.Schema(
     {
         firstName: { type: String, required: true, trim: true, minlength: 2 },
-        lastName: { type: String, required: true, trim: true, minlength: 2 },
+        lastName: { type: String, required: true, trim: true, minlength: 1 },
         email: { type: String, required: true, lowercase: true, unique: true, trim: true },
         passwordHash: { type: String, required: true}
     },
@@ -15,7 +15,7 @@ const User = mongoose.model('User', userSchema);
 
 const registerSchema = Joi.object({
     firstName: Joi.string().min(2).max(100).required(),
-    lastName: Joi.string().min(2).max(100).required(),
+    lastName: Joi.string().min(1).max(100).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required()
 });
